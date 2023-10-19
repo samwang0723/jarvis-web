d7c9c23a79704acc335a510847623762773aa1cc
Sam Wang <sam.wang.0723@gmail.com>
2023-10-19 22:47:57 +0800

    feat: adjust layout

--- src/_theme/layout/MasterLayout.tsx
+++ src/_theme/layout/MasterLayout.tsx
@@ -1,56 +1,62 @@
-import React, {useEffect} from 'react'
-import {PageDataProvider} from './core'
-import {Outlet, useLocation} from 'react-router-dom'
-import {MenuComponent} from '../assets/ts/components'
-import HeaderBase from './components/header/HeaderBase'
-import Footer from './components/Footer'
-import AsideBase from './components/aside/AsideBase'
-import {ScrollTop} from './components/Scrolltop'
-import {ActivityDrawer, CreateApp, DrawerMessenger} from '../partials'
-import {Content} from './components/Content'
+import React, { useEffect } from "react";
+import { PageDataProvider } from "./core";
+import { Outlet, useLocation } from "react-router-dom";
+import { MenuComponent } from "../assets/ts/components";
+import HeaderBase from "./components/header/HeaderBase";
+import Footer from "./components/Footer";
+import AsideBase from "./components/aside/AsideBase";
+import { ScrollTop } from "./components/Scrolltop";
+import { ActivityDrawer, CreateApp, DrawerMessenger } from "../partials";
+import { Content } from "./components/Content";
 
-const MasterLayout: React.FC = ({children}) => {
-  const location = useLocation()
-  useEffect(() => {
-    setTimeout(() => {
-      MenuComponent.reinitialization()
-    }, 500)
-  }, [])
+const MasterLayout: React.FC = ({ children }) => {
+    const location = useLocation();
+    useEffect(() => {
+        setTimeout(() => {
+            MenuComponent.reinitialization();
+        }, 500);
+    }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      MenuComponent.reinitialization()
-    }, 500)
-  }, [location.key])
+    useEffect(() => {
+        setTimeout(() => {
+            MenuComponent.reinitialization();
+        }, 500);
+    }, [location.key]);
 
-  return (
-    <PageDataProvider>
-      <div className='d-flex flex-column flex-root'>
-        <div className='page d-flex flex-row flex-column-fluid'>
-          <AsideBase />
+    return (
+        <PageDataProvider>
+            <div className="d-flex flex-column flex-root">
+                <div className="page d-flex flex-row flex-column-fluid">
+                    <AsideBase />
 
-          <div className='wrapper d-flex flex-column flex-row-fluid' id='kt_wrapper'>
-            <HeaderBase />
+                    <div
+                        className="wrapper d-flex flex-column flex-row-fluid"
+                        id="kt_wrapper"
+                    >
+                        <HeaderBase />
 
-            <div className='content d-flex flex-column flex-column-fluid' id='kt_content'>
-              <Content>
-                <Outlet />
-              </Content>
+                        <div
+                            className="content d-flex flex-column flex-column-fluid"
+                            id="kt_content"
+                        >
+                            <Content>
+                                <Outlet />
+                            </Content>
+                        </div>
+
+                        <Footer />
+                    </div>
+                </div>
             </div>
+            {/* begin:: Drawers */}
+            <ActivityDrawer />
+            <DrawerMessenger />
+            {/* end:: Drawers */}
 
-            <Footer />
-          </div>
-        </div>
-      </div>
-      {/* begin:: Drawers */}
-      <ActivityDrawer />
-      <DrawerMessenger />
-      {/* end:: Drawers */}
+            <CreateApp />
+            <ScrollTop />
+        </PageDataProvider>
+    );
+};
 
-      <CreateApp />
-      <ScrollTop />
-    </PageDataProvider>
-  )
-}
-
-export {MasterLayout}
+export { MasterLayout };
--- src/app/pages/dashboards/DashboardsPageWrapper.tsx
+++ src/app/pages/dashboards/DashboardsPageWrapper.tsx
@@ -1,70 +1,74 @@
-import React from 'react'
-import {Navigate, Outlet, Route, Routes} from 'react-router-dom'
-import {PageLink, PageTitle} from '../../../_theme/layout/core'
-import {Default} from './Default'
-import {Marketing} from './Marketing'
-import {Social} from './Social'
-import {ECommerce} from './eCommerce'
+import React from "react";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { PageLink, PageTitle } from "../../../_theme/layout/core";
+import { Default } from "./Default";
+import { Marketing } from "./Marketing";
+import { Social } from "./Social";
+import { ECommerce } from "./eCommerce";
 
 const dashboardBreadCrumbs: Array<PageLink> = [
-  {
-    title: 'Dashboards',
-    path: '/dashboards',
-    isSeparator: false,
-    isActive: false,
-  },
-  {
-    title: '',
-    path: '',
-    isSeparator: true,
-    isActive: false,
-  },
-]
+    {
+        title: "Dashboards",
+        path: "/dashboards",
+        isSeparator: false,
+        isActive: false,
+    },
+    {
+        title: "",
+        path: "",
+        isSeparator: true,
+        isActive: false,
+    },
+];
 
 const DashboardsPageWrapper: React.FC = () => {
-  return (
-    <Routes>
-      <Route element={<Outlet />}>
-        <Route
-          path='default'
-          element={
-            <>
-              <PageTitle breadcrumbs={dashboardBreadCrumbs}>Default</PageTitle>
-              <Default />
-            </>
-          }
-        />
-        <Route
-          path='marketing'
-          element={
-            <>
-              <PageTitle breadcrumbs={dashboardBreadCrumbs}>Marketing</PageTitle>
-              <Marketing />
-            </>
-          }
-        />
-        <Route
-          path='/social'
-          element={
-            <>
-              <PageTitle breadcrumbs={dashboardBreadCrumbs}>Social</PageTitle>
-              <Social />
-            </>
-          }
-        />
-        <Route
-          path='ecommerce'
-          element={
-            <>
-              <PageTitle breadcrumbs={dashboardBreadCrumbs}>ECommerce</PageTitle>
-              <ECommerce />
-            </>
-          }
-        />
-        <Route index element={<Navigate to='/dashboards/default' />} />
-      </Route>
-    </Routes>
-  )
-}
+    return (
+        <Routes>
+            <Route element={<Outlet />}>
+                <Route
+                    path="default"
+                    element={
+                        <>
+                            <PageTitle breadcrumbs={dashboardBreadCrumbs}>Default</PageTitle>
+                            <Default />
+                        </>
+                    }
+                />
+                <Route
+                    path="marketing"
+                    element={
+                        <>
+                            <PageTitle breadcrumbs={dashboardBreadCrumbs}>
+                                Marketing
+                            </PageTitle>
+                            <Marketing />
+                        </>
+                    }
+                />
+                <Route
+                    path="/social"
+                    element={
+                        <>
+                            <PageTitle breadcrumbs={dashboardBreadCrumbs}>Social</PageTitle>
+                            <Social />
+                        </>
+                    }
+                />
+                <Route
+                    path="ecommerce"
+                    element={
+                        <>
+                            <PageTitle breadcrumbs={dashboardBreadCrumbs}>
+                                ECommerce
+                            </PageTitle>
+                            <ECommerce />
+                        </>
+                    }
+                />
+                <Route index element={<Navigate to="/dashboards/marketing" />} />
+            </Route>
+        </Routes>
+    );
+};
 
-export {DashboardsPageWrapper}
+export { DashboardsPageWrapper };
