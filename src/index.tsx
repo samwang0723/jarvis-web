1d562f6b4a6dd0cd42bf42a01544ef43ac0bb1b0
Sam Wang <sam.wang.0723@gmail.com>
2023-10-20 14:05:16 +0800

    fix: remove the react debug button

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 // Redux
 // https://github.com/rt2zz/redux-persist
-import {PersistGate} from 'redux-persist/integration/react'
-import {Provider} from 'react-redux'
-import * as _redux from './setup'
-import store, {persistor} from './setup/redux/Store'
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import * as _redux from "./setup";
+import store, { persistor } from "./setup/redux/Store";
 // Axios
-import axios from 'axios'
-import {Chart, registerables} from 'chart.js'
-import {QueryClient, QueryClientProvider} from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
+import axios from "axios";
+import { Chart, registerables } from "chart.js";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // Apps
-import {GoodI18nProvider} from './_theme/i18n/Goodi18n'
+import { GoodI18nProvider } from "./_theme/i18n/Goodi18n";
 /**
  * TIP: Replace this style import with dark styles to enable dark mode
  *
@@ -22,33 +21,32 @@ import {GoodI18nProvider} from './_theme/i18n/Goodi18n'
  *
  * import './_theme/assets/css/style.rtl.css'
  **/
-import './_theme/assets/sass/style.scss'
-import './_theme/assets/sass/plugins.scss'
-import './_theme/assets/sass/style.react.scss'
-import {AppRoutes} from './app/routing/AppRoutes'
+import "./_theme/assets/sass/style.scss";
+import "./_theme/assets/sass/plugins.scss";
+import "./_theme/assets/sass/style.react.scss";
+import { AppRoutes } from "./app/routing/AppRoutes";
 
 /**
  * Inject Good interceptors for axios.
  *
  * @see https://github.com/axios/axios#interceptors
  */
-_redux.setupAxios(axios, store)
+_redux.setupAxios(axios, store);
 
-Chart.register(...registerables)
+Chart.register(...registerables);
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <GoodI18nProvider>
-      <Provider store={store}>
-        {/* Asynchronously persist redux stores and show `SplashScreen` while it's loading. */}
-        <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
-          <AppRoutes />
-        </PersistGate>
-      </Provider>
-    </GoodI18nProvider>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>,
-  document.getElementById('root')
-)
+    <QueryClientProvider client={queryClient}>
+        <GoodI18nProvider>
+            <Provider store={store}>
+                {/* Asynchronously persist redux stores and show `SplashScreen` while it's loading. */}
+                <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+                    <AppRoutes />
+                </PersistGate>
+            </Provider>
+        </GoodI18nProvider>
+    </QueryClientProvider>,
+    document.getElementById("root"),
+);
